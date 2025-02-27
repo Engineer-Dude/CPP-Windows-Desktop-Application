206783983c0f8ebce3c53913ddb1502d83a046bb
Paul Phillips <98303277+Engineer-Dude@users.noreply.github.com>
2025-02-27 11:58:02 -0800

    Initial commit.  Added 2 Labels, a Button, a TextBox, and a png file.

---
+++ CPP_Desktop.cpp
@@ -0,0 +1,312 @@
+// CPP_Desktop.cpp : Defines the entry point for the application.
+//
+
+#include "framework.h"
+#include "CPP_Desktop.h"
+
+#include <wincodec.h>
+#pragma comment(lib, "windowscodecs.lib")
+
+#include <gdiplus.h>
+#pragma comment(lib, "gdiplus.lib")
+
+#define MAX_LOADSTRING 100
+
+// Global Variables:
+HINSTANCE hInst;                                // current instance
+WCHAR szTitle[MAX_LOADSTRING];                  // The title bar text
+WCHAR szWindowClass[MAX_LOADSTRING];            // the main window class name
+HBRUSH hbrWindowBackground;
+IWICStream* pStream = nullptr;
+Gdiplus::GdiplusStartupInput gdiplusStartupInput;
+ULONG_PTR gdiplusToken;
+Gdiplus::Status status = Gdiplus::GdiplusStartup(&gdiplusToken, &gdiplusStartupInput, nullptr);
+
+// Forward declarations of functions included in this code module:
+ATOM                MyRegisterClass(HINSTANCE hInstance);
+BOOL                InitInstance(HINSTANCE, int);
+LRESULT CALLBACK    WndProc(HWND, UINT, WPARAM, LPARAM);
+INT_PTR CALLBACK    About(HWND, UINT, WPARAM, LPARAM);
+void LoadAndDisplayPng(HWND hwndParent);
+
+int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
+	_In_opt_ HINSTANCE hPrevInstance,
+	_In_ LPWSTR    lpCmdLine,
+	_In_ int       nCmdShow)
+{
+	UNREFERENCED_PARAMETER(hPrevInstance);
+	UNREFERENCED_PARAMETER(lpCmdLine);
+
+	if (status != Gdiplus::Ok)
+	{
+		MessageBox(nullptr, TEXT("Failed to initialize GDI+"), TEXT("Error"), MB_OK);
+		return -1;
+	}
+
+	// TODO: Place code here.
+
+
+	// Initialize global strings
+	LoadStringW(hInstance, IDS_APP_TITLE, szTitle, MAX_LOADSTRING);
+	LoadStringW(hInstance, IDC_CPPDESKTOP, szWindowClass, MAX_LOADSTRING);
+	MyRegisterClass(hInstance);
+
+	// Perform application initialization:
+	if (!InitInstance(hInstance, nCmdShow))
+	{
+		return FALSE;
+	}
+
+	HACCEL hAccelTable = LoadAccelerators(hInstance, MAKEINTRESOURCE(IDC_CPPDESKTOP));
+
+	MSG msg;
+
+	// Main message loop:
+	while (GetMessage(&msg, nullptr, 0, 0))
+	{
+		if (!TranslateAccelerator(msg.hwnd, hAccelTable, &msg))
+		{
+			TranslateMessage(&msg);
+			DispatchMessage(&msg);
+		}
+	}
+
+	Gdiplus::GdiplusShutdown(gdiplusToken);
+
+	return (int)msg.wParam;
+}
+
+//
+//  FUNCTION: MyRegisterClass()
+//
+//  PURPOSE: Registers the window class.
+//
+ATOM MyRegisterClass(HINSTANCE hInstance)
+{
+	WNDCLASSEXW wcex;
+
+	wcex.cbSize = sizeof(WNDCLASSEX);
+
+	wcex.style = CS_HREDRAW | CS_VREDRAW;
+	wcex.lpfnWndProc = WndProc;
+	wcex.cbClsExtra = 0;
+	wcex.cbWndExtra = 0;
+	wcex.hInstance = hInstance;
+	wcex.hIcon = LoadIcon(hInstance, MAKEINTRESOURCE(IDI_CPPDESKTOP));
+	wcex.hCursor = LoadCursor(nullptr, IDC_ARROW);
+	wcex.hbrBackground = (HBRUSH)(COLOR_WINDOW + 1);
+	wcex.lpszMenuName = MAKEINTRESOURCEW(IDC_CPPDESKTOP);
+	wcex.lpszClassName = szWindowClass;
+	wcex.hIconSm = LoadIcon(wcex.hInstance, MAKEINTRESOURCE(IDI_SMALL));
+
+	return RegisterClassExW(&wcex);
+}
+
+//
+//   FUNCTION: InitInstance(HINSTANCE, int)
+//
+//   PURPOSE: Saves instance handle and creates main window
+//
+//   COMMENTS:
+//
+//        In this function, we save the instance handle in a global variable and
+//        create and display the main program window.
+//
+BOOL InitInstance(HINSTANCE hInstance, int nCmdShow)
+{
+	hInst = hInstance; // Store instance handle in our global variable
+
+	HWND hWnd = CreateWindowW(szWindowClass, szTitle, WS_OVERLAPPEDWINDOW,
+		CW_USEDEFAULT, 0, CW_USEDEFAULT, 0, nullptr, nullptr, hInstance, nullptr);
+
+	if (!hWnd)
+	{
+		return FALSE;
+	}
+
+	ShowWindow(hWnd, nCmdShow);
+	UpdateWindow(hWnd);
+
+	return TRUE;
+}
+
+//
+//  FUNCTION: WndProc(HWND, UINT, WPARAM, LPARAM)
+//
+//  PURPOSE: Processes messages for the main window.
+//
+//  WM_COMMAND  - process the application menu
+//  WM_PAINT    - Paint the main window
+//  WM_DESTROY  - post a quit message and return
+//
+//
+LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
+{
+	switch (message)
+	{
+	case WM_CREATE: {
+		hbrWindowBackground = CreateSolidBrush(RGB(13, 79, 132));
+
+		HWND hLabel = CreateWindow(
+			TEXT("STATIC"),				// Predefined class for a label
+			TEXT("Hello, world!"),		// Text to display
+			WS_CHILD | WS_VISIBLE,		// Styles
+			10, 10, 200, 20,			// Position and size
+			hWnd,						// Parent window
+			NULL,						// No menu
+			(HINSTANCE)GetWindowLongPtr(hWnd, GWLP_HINSTANCE),
+			NULL						// No additional parameters
+		);
+
+		HWND hwndButton = CreateWindow(
+			TEXT("BUTTON"),											// Predefined class
+			TEXT("Click Me"),										// Button text
+			WS_TABSTOP | WS_VISIBLE | WS_CHILD | BS_DEFPUSHBUTTON,	// Styles
+			10,														// X-position
+			40,														// Y-position
+			100,													// Width
+			30,														// Height
+			hWnd,													// Parent window
+			(HMENU)ID_BUTTON1,												// No menu
+			(HINSTANCE)GetWindowLongPtr(hWnd, GWLP_HINSTANCE),
+			NULL													// Pointer not needed
+		);
+
+		HWND hLabelTextBox = CreateWindow(
+			TEXT("STATIC"),				// Predefined class for a label
+			TEXT("Enter a value"),		// Text to display
+			WS_CHILD | WS_VISIBLE,		// Styles
+			10, 85, 100, 20,			// Position and size
+			hWnd,						// Parent window
+			NULL,						// No menu
+			(HINSTANCE)GetWindowLongPtr(hWnd, GWLP_HINSTANCE),
+			NULL						// No additional parameters
+		);
+
+		HWND hwndTextBox = CreateWindowEx(
+			WS_EX_CLIENTEDGE,				// Extended window style
+			TEXT("EDIT"),					// Predefined class
+			NULL,							// No default text
+			WS_CHILD | WS_VISIBLE | WS_BORDER | ES_LEFT,	// Styles
+			180,								// X-position
+			80,								// Y-position
+			200,							// Width
+			30,								// Height
+			hWnd,							// Parent window
+			(HMENU)ID_TEXTBOX1,				// Control ID
+			(HINSTANCE)GetWindowLongPtr(hWnd, GWLP_HINSTANCE),
+			NULL							// Pointer not needed
+		);
+
+		LoadAndDisplayPng(hWnd);
+
+		UpdateWindow(hWnd);
+
+		break;
+	}
+
+	case WM_ERASEBKGND:
+	{
+		HDC hdc = (HDC)wParam;
+		RECT rc;
+		GetClientRect(hWnd, &rc);
+		FillRect(hdc, &rc, hbrWindowBackground);
+		return 1;
+	}
+
+	case WM_COMMAND:
+	{
+		int wmId = LOWORD(wParam);
+		// Parse the menu selections:
+		switch (wmId)
+		{
+		case IDM_ABOUT:
+			DialogBox(hInst, MAKEINTRESOURCE(IDD_ABOUTBOX), hWnd, About);
+			break;
+		case IDM_EXIT:
+			DestroyWindow(hWnd);
+			break;
+		case ID_TEXTBOX1:
+			if (HIWORD(wParam) == EN_SETFOCUS)
+			{
+				MessageBox(hWnd, TEXT("Text Box 1 was pressed"), TEXT("Notification"), MB_OK);
+			}
+			break;
+		case ID_BUTTON1:
+			MessageBox(hWnd, TEXT("Button (click me) was pressed"), TEXT("Notification"), MB_OK);
+			break;
+
+		default:
+			return DefWindowProc(hWnd, message, wParam, lParam);
+		}
+	}
+	break;
+	case WM_PAINT:
+	{
+		PAINTSTRUCT ps;
+		HDC hdc = BeginPaint(hWnd, &ps);
+
+		// TODO: Add any drawing code that uses hdc here...
+		Gdiplus::Graphics graphics(hdc);
+		Gdiplus::Bitmap* pBitmap = Gdiplus::Bitmap::FromStream(pStream);
+		graphics.DrawImage(pBitmap, 0, 0);
+		delete pBitmap;
+
+		EndPaint(hWnd, &ps);
+		break;
+	}
+	case WM_DESTROY:
+		PostQuitMessage(0);
+		break;
+	default:
+		return DefWindowProc(hWnd, message, wParam, lParam);
+	}
+	return 0;
+}
+
+// Message handler for about box.
+INT_PTR CALLBACK About(HWND hDlg, UINT message, WPARAM wParam, LPARAM lParam)
+{
+	UNREFERENCED_PARAMETER(lParam);
+	switch (message)
+	{
+	case WM_INITDIALOG:
+		return (INT_PTR)TRUE;
+
+	case WM_COMMAND:
+		if (LOWORD(wParam) == IDOK || LOWORD(wParam) == IDCANCEL)
+		{
+			EndDialog(hDlg, LOWORD(wParam));
+			return (INT_PTR)TRUE;
+		}
+		break;
+	}
+	return (INT_PTR)FALSE;
+}
+
+void LoadAndDisplayPng(HWND hwndParent)
+{
+	IWICImagingFactory* pFactory = nullptr;
+	IWICBitmapDecoder* pDecoder = nullptr;
+	IWICBitmapFrameDecode* pFrame = nullptr;
+	pStream = nullptr;
+	HRSRC hResource = FindResource(hInst, MAKEINTRESOURCE(IDR_PNG_LIGHTBULB), TEXT("PNG"));
+
+	if (hResource)
+	{
+		HGLOBAL hMemory = LoadResource(hInst, hResource);
+		DWORD imageSize = SizeofResource(hInst, hResource);
+		void* pResourceData = LockResource(hMemory);
+
+		CoInitialize(nullptr);
+		CoCreateInstance(CLSID_WICImagingFactory, nullptr, CLSCTX_INPROC_SERVER, IID_PPV_ARGS(&pFactory));
+		pFactory->CreateStream(&pStream);
+		pStream->InitializeFromMemory(reinterpret_cast<BYTE*>(pResourceData), imageSize);
+		pFactory->CreateDecoderFromStream(pStream, nullptr, WICDecodeMetadataCacheOnLoad, &pDecoder);
+		pDecoder->GetFrame(0, &pFrame);
+	}
+	else
+	{
+		MessageBox(hwndParent, TEXT("Error finding resource"), TEXT("Error"), MB_OK);
+	}
+}
---
+++ resource.h
@@ -0,0 +1,34 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ generated include file.
+// Used by CPP_Desktop.rc
+//
+#define IDC_MYICON                      2
+#define IDD_CPPDESKTOP_DIALOG           102
+#define IDS_APP_TITLE                   103
+#define IDD_ABOUTBOX                    103
+#define IDM_ABOUT                       104
+#define IDM_EXIT                        105
+#define IDI_CPPDESKTOP                  107
+#define IDI_SMALL                       108
+#define IDC_CPPDESKTOP                  109
+#define IDR_MAINFRAME                   128
+#define IDR_ACCELERATOR1                131
+#define IDR_ACCELERATOR2                134
+#define IDD_DIALOG1                     135
+#define ID_BUTTON1                      136
+#define ID_TEXTBOX1                     137
+#define IDB_PNG1                        140
+#define IDR_PNG_LIGHTBULB               141
+#define IDC_STATIC                      -1
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NO_MFC                     1
+#define _APS_NEXT_RESOURCE_VALUE        142
+#define _APS_NEXT_COMMAND_VALUE         32773
+#define _APS_NEXT_CONTROL_VALUE         1000
+#define _APS_NEXT_SYMED_VALUE           110
+#endif
+#endif
