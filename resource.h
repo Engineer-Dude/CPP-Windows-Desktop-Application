e933754e76d4b308f6a73df3fb380dd1084ad76c
Paul Phillips <98303277+Engineer-Dude@users.noreply.github.com>
2025-02-27 15:19:35 -0800

    Updated icon.

--- resource.h
+++ resource.h
@@ -26,7 +26,7 @@
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
 #define _APS_NO_MFC                     1
-#define _APS_NEXT_RESOURCE_VALUE        142
+#define _APS_NEXT_RESOURCE_VALUE        144
 #define _APS_NEXT_COMMAND_VALUE         32773
 #define _APS_NEXT_CONTROL_VALUE         1000
 #define _APS_NEXT_SYMED_VALUE           110
